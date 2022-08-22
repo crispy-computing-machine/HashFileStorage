545461855cf5b7f862b4e8a8775532919a20a11e
Stefan <Stefan>
2022-08-22 09:26:47 +0100

    composer.json

--- HashFileStorage.php
+++ HashFileStorage.php
@@ -5,28 +5,6 @@ namespace CoreLibrarys;
  *
  * Hash table / hash bucket / hash map
  *
- * Folder file storage limits:.
- *
- *  Windows FAT32 = 65,534
- *  Windows NTFS = 4,294,967,295 (unlimited)
- *  Max files using hash storage:
- *  one hundred and thirty-seven billion,
- *  four hundred and thirty-eight million,
- *  nine hundred and fifty-three thousand,
- *  four hundred and forty
- *
- *  Linux (uname -r to find ext):
- *  ext2/ext3 = 10,000
- *  ext4 = unlimited
- *
- * // Test storage of multi files in multi folders
- * $oStorage = new HashFileStorage();
- * for($i = 0; $i <= 1000; $i++){
- * $sTestFilename = uniqid("store_");
- * $sTestFileText = "This file contains text for file: " . $sTestFilename;
- * $bSave = $oStorage->file_put_contents($sTestFilename, $sTestFileText);
- * error_log("Saving HashFileStorage file: " . $sTestFilename . " (" . var_export($bSave, TRUE) . ")");
- * }
  */
 class HashFileStorage
 {
@@ -60,6 +38,7 @@ class HashFileStorage
         try {
             $this->setBaseDirectory($sStoragePath);
         } catch (ErrorException $e) {
+            /** @noinspection ForgottenDebugOutputInspection */
             error_log($e);
         }
         $this->setSubdirectoryLevels($iSubdirectoryLevels);
